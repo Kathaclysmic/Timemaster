c966ea1298b3df7cfc23df99abc630eaa0653c2a
Kathaclysm <katharina.mundt@student.uni-luebeck.de>
2023-05-24 06:49:04 +0200

    Added _redirects file

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Menubar from "./components/Menubar.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <menubar></menubar>
+    <div class="notification is-danger">
+      This app is a work in progress. Most features have yet to be implemented and the UI will be subject to change.
+    </div>
+    <router-view/>
+  </div>
 </template>
+
+<script>
+
+</script>
+
 
 <style>
 #app {
@@ -17,5 +28,7 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  z-index: 0;
+  padding: 2rem;
 }
 </style>
